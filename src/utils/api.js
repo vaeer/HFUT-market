2035ad2d1a1400ae89141ea05c8516d7f91d5cfd
Jing <731088324@qq.com>
2018-03-16 13:22:38 +0800

    第一次提交

---
+++ src/utils/api.js
@@ -0,0 +1,14 @@
+var HOST_URL = "https://127.0.0.1:8000";
+
+
+//通过函数获取url
+
+//登录
+function _LOGIN() {
+    return HOST_URL + "/userLogin";
+}
+
+//注册
+function _Register() {
+    return HOST_URL + "/userRegister";
+}
